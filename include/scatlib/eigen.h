4fc6102a0f71a3b6aff33b64ba6aaccc0f5dce53
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-08-05 18:28:34 +0200

    Working on scattering data format.

--- include/scatlib/eigen.h
+++ include/scatlib/eigen.h
@@ -1,6 +1,6 @@
 /** \file eigen.h
  *
- * Type alias for Eigen types.
+ * Type aliases for Eigen types.
  *
  * @author Simon Pfreundschuh, 2020
  */
@@ -15,16 +15,53 @@ namespace eigen {
 
 using Index = Eigen::Index;
 
+//
+// Vectors
+//
+
+/** Variable-length vector.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
 template <typename Scalar>
 using Vector = Eigen::Matrix<Scalar, 1, -1, Eigen::RowMajor>;
+/** Fixed-length vector.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
 template <typename Scalar, size_t N>
 using VectorFixedSize = Eigen::Matrix<Scalar, 1, N, Eigen::RowMajor>;
+/** Variable-length vector that doesn't own its data.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
+template <typename Scalar>
+using VectorMap = Eigen::Map<Vector<Scalar>>;
+
+//
+// Matrices
+//
+
+/** A variable-size matrix containing coefficients of type Scalar.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
 template <typename Scalar>
 using Matrix = Eigen::Matrix<Scalar, -1, -1, Eigen::RowMajor>;
 template <typename Scalar, size_t N>
+/** Matrix with fixed number of rows.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
 using MatrixFixedRows = Eigen::Matrix<Scalar, -1, N, Eigen::RowMajor>;
+/** A matrix that doesn't own its data.
+ * @tparam Scalar The type used to represent coefficients of the matrix.
+ */
+template <typename Scalar>
+using MatrixMap = Eigen::Map<Matrix<Scalar>>;
+
+//
+// Tensors
+//
+
 template <typename Scalar, size_t rank>
 using Tensor = Eigen::Tensor<Scalar, rank, Eigen::RowMajor>;
+
 }  // namespace eigen
 }  // namespace scatlib
 
--- include/scatlib/scattering_data.h
+++ include/scatlib/scattering_data.h
@@ -9,59 +9,29 @@
 #define __SCATLIB_SCATTERING_DATA__
 
 #include <scatlib/eigen.h>
-
+#include <scatlib/sht.h>
 
 namespace scatlib {
 
-template <Scalar, size_t rank>
-    using SpectralTensorType = eigen::Tensor<std::complex<Scalar>, rank>;
-template <Scalar, size_t rank>
-    using GriddedTensorType = eigen::Tensor<Scalar>,
-    rank > ;
-
-enum class DataFormat { Gridded, Spectral };
+template <typename Scalar, size_t rank>
+using SpectralTensorType = eigen::Tensor<std::complex<Scalar>, rank>;
+template <typename Scalar, size_t rank>
+using GriddedTensorType = eigen::Tensor<Scalar, rank>;
 
 namespace detail {
 
-/** Transform fully-gridded to semi-spectral representation.
+/** Transform fully-gridded to spectral representation.
  * Class to transform fully-gridded scattering data to spectral
  * format along the scattering angles.
  */
 template <typename Scalar>
-class ScatteringToSpectralTransformer {
-  /// Map pointing to scattering-angle field in input tensor.
-  template <size_t rank>
-  static eigen::MatrixMap<Scalar> get_input_sequence(
-      size_t major_index, GriddedTensorType<Scalar, rank> &in) {
-    size_t start = major_index * get_major_stride_in();
-    return MatrixMap<Scalar>(in.data, n_lon_, n_lat_);
-  }
+class GriddedToSpectralTransformer {
 
-  /// Map pointing to spherical harmonics coefficient vector in input tensor.
-  template <size_t rank>
-  static eigen::VectorMap<std::complex<Scalar>> get_output_sequence(
-      size_t major_index, SpectralTensorType<Scalar, rank - 1> &out) {
-    size_t start = major_index * get_major_stride_out();
-    size_t stride = get_lm_stride_out();
-    return VectorMap<Scalar>(out.data, nlm_inc_, {stride});
-  }
-
-  /// Dimensions of the output (transformed) tensor.
-  template <size_t rank>
-  static std::array<size_t, rank - 1> get_output_dimensions(
-      const GriddedTensorType<Scalar, rank> &in) {
-    auto input_size = in.dimensions();
-    std::array<size_t, rank - 1> output_dimensions;
-    std::copy(input_dimensions.begin(), input_dimensions.end() - 1,
-              output_dimensions.begin());
-    output_dimensions[rank - 1] = nlm;
-    return output_dimensions;
-  }
   /** Initialize transformer.
    *
    * @param t The input tensor to transform.
    */
-  ScatteringToSpectralTransformer(SHT &sht)
+GriddedToSpectralTransformer(scatlib::sht::SHT &sht)
       : sht_(sht),
         n_lat_(sht.get_size_of_colatitude_grid()),
         n_lon_(sht.get_size_of_longitude_grid()),
@@ -83,32 +53,64 @@ class ScatteringToSpectralTransformer {
    */
   size_t get_major_stride_out() { return nlm_scat_; }
 
+  /// Map pointing to the two-dimensions coefficient field for given scattering angles.
+  template <size_t rank>
+  eigen::MatrixMap<Scalar> get_input_sequence(
+      size_t major_index,
+      GriddedTensorType<Scalar, rank> &in) {
+    size_t start = major_index * get_major_stride_in();
+    return eigen::MatrixMap<Scalar>(in.data, n_lon_, n_lat_);
+  }
+
+  /// Map pointing to spherical harmonics coefficient vector in input tensor.
+  template <size_t rank>
+  eigen::VectorMap<std::complex<Scalar>> get_output_sequence(
+      size_t major_index, SpectralTensorType<Scalar, rank - 1> &out) {
+    size_t start = major_index * get_major_stride_out();
+    return eigen::VectorMap<Scalar>(out.data, nlm_scat_);
+  }
+
+  /// Dimensions of the output (transformed) tensor.
+  template <size_t rank>
+  std::array<size_t, rank - 1> get_output_dimensions(
+      const GriddedTensorType<Scalar, rank> &in) {
+    auto input_dimensions = in.dimensions();
+    std::array<size_t, rank - 1> output_dimensions;
+    std::copy(input_dimensions.begin(), input_dimensions.end() - 1,
+              output_dimensions.begin());
+    output_dimensions[rank - 1] = nlm_scat_;
+    return output_dimensions;
+  }
+
   template <size_t rank>
   SpectralTensorType<Scalar, rank - 1> transform(
       const SpectralTensorType<Scalar, rank> &in) {
     SpectralTensorType<Scalar, rank - 1> out(get_output_dimensions());
-    for (size_t i = 0; i < out.size() / get_major_stride(); ++i) {
+    for (size_t i = 0; i < out.size() / get_major_stride_out(); ++i) {
       get_output_sequence(i, out) = get_input_sequence(i, in);
     }
     return out;
   }
 
  protected:
-  SHT &sht;
+  scatlib::sht::SHT &sht_;
   size_t n_lat_, n_lon_, nlm_scat_;
 };
 
-/** Transform semi-spectral to fully-gridded.
- * Class to transform semi-spectral scattering data to fully-spectral
+/** Transform spectral to fully-spectral.
+ *
+ * Class to transform spectral scattering data to fully-spectral
  * format.
+ *
  */
 template <typename Scalar>
-class IncomingToSpectralTransformer {
-  IncomingToSpectralTransformer(const SpectralTensorType<Scalar, rank> &t,
-                                SHT &sht) sht_(sht),
-      n_lat_(sht.get_size_of_colatitude_grid()),
-      n_lon_(sht.get_size_of_longitude_grid()),
-      nlm_scat_(sht.get_number_of_spectral_coefficient()) {}
+class SpectralToFullySpectralTransformer {
+    SpectralToFullySpectralTransformer(size_t nlm_scat, scatlib::sht::SHT &sht)
+      : sht_(sht),
+        n_lat_(sht.get_size_of_colatitude_grid()),
+        n_lon_(sht.get_size_of_longitude_grid()),
+        nlm_scat_(nlm_scat),
+        nlm_inc_(sht.get_number_of_spectral_coefficients()) {}
 
   /**
    * The stride between sequences of SH coefficient over incoming angles
@@ -134,48 +136,60 @@ class IncomingToSpectralTransformer {
   size_t get_lm_stride_out() { return nlm_scat_; }
 
   /**
-   * Matrix map pointing to sequence of scattering SH coefficients in the input tensor.
+   * Matrix map pointing to sequence of scattering SH coefficients in the input
+   * tensor.
    */
-  template<size_t rank>
-  eigen::MatrixMap<Scalar> get_input_sequence(size_t major_index,
-                                              size_t lm_index,
-                                              size_t complex_index,
-                                              SpectralTensorType<Scalar, rank> &in) {
+  template <size_t rank>
+  eigen::MatrixMap<Scalar> get_input_sequence(
+      size_t major_index,
+      size_t lm_index,
+      size_t complex_index,
+      SpectralTensorType<Scalar, rank> &in) {
     size_t start = major_index * get_major_stride_in() +
                    lm_index * get_lm_stride_in() + complex_index;
     size_t col_stride = 2 * get_lm_stride_in();
     size_t row_stride = 2 * get_lm_stride_in() * n_lat_;
-    return MatrixMap<Scalar>(in.data, n_lon_, n_lat_,
-                             {row_stride, col_stride});
+    return eigen::MatrixMap<Scalar>(in.data,
+                                    n_lon_,
+                                    n_lat_,
+                                    {row_stride, col_stride});
   }
 
   /**
-   * Vector map pointing to sequences of incoming SH coefficients in the input tensor.
+   * Vector map pointing to sequences of incoming SH coefficients in the input
+   * tensor.
    */
+  template <size_t rank>
   eigen::VectorMap<std::complex<Scalar>> get_output_sequence(
-      size_t major_index, size_t lm_index, size_t complex_index,
+      size_t major_index,
+      size_t lm_index,
+      size_t complex_index,
       SpectralTensorType<Scalar, rank> &out) {
     size_t start = major_index * get_major_stride_out() +
                    complex_index * get_major_stride_out() / 2 +
                    lm_index * get_lm_stride_out();
     size_t stride = get_lm_stride_out();
-    return VectorMap<Scalar>(out.data, nlm_inc_, {stride});
+    return eigen::VectorMap<Scalar>(out.data, nlm_inc_, {stride});
   }
 
   /// Dimensions of the output (transformed) tensor.
-  std::array<size_t, rank> get_output_dimensions(SpectralTensorType<Scalar, rank> &in) {
-    auto input_size = in.dimensions();
+  template <size_t rank>
+  std::array<size_t, rank> get_output_dimensions(
+      SpectralTensorType<Scalar, rank> &in) {
+    auto input_dimensions = in.dimensions();
     std::array<size_t, rank> output_dimensions;
-    std::copy(input_dimensions.begin(), input_dimensions.end(),
+    std::copy(input_dimensions.begin(),
+              input_dimensions.end(),
               output_dimensions.begin());
-    output_dimensions[rank - 3] = nlm;
+    output_dimensions[rank - 3] = nlm_inc_;
     output_dimensions[rank - 4] = 2;
     return output_dimensions;
   }
 
+  template <size_t rank>
   SpectralTensorType<Scalar, rank> transform() {
     SpectralTensorType<Scalar, rank> out(get_output_dimensions());
-    for (size_t i = 0; i < out.size() / get_major_stride(); ++i) {
+    for (size_t i = 0; i < out.size() / get_major_stride_out(); ++i) {
       for (size_t j = 0; j < nlm_scat_; ++j) {
         get_output_sequence(i, j, 0, out) = get_input_sequence(i, j, 0);
         get_output_sequence(i, j, 1, out) = get_input_sequence(i, j, 1);
@@ -185,11 +199,21 @@ class IncomingToSpectralTransformer {
   }
 
  protected:
-  SHT &sht;
-  size_t n_lat_, n_lon_, nlm_scat_;
+  scatlib::sht::SHT &sht_;
+  size_t n_lat_, n_lon_, nlm_scat_, nlm_inc_;
 };
 }  // namespace detail
 
+//
+// Scattering data.
+//
+
+enum class DataFormat {Gridded, Spectral, FullySpectral};
+
+
+/** Base class for scattering data.
+ * Holds frequency and temperature grids.
+ */
 template <typename Scalar>
 class ScatteringDataBase {
  public:
@@ -202,8 +226,10 @@ class ScatteringDataBase {
   eigen::Vector<Scalar> temperature_grid_;
 };
 
+template <typename Scalar, DataFormat format> class ScatteringData;
+
 template <typename Scalar>
-class ScatteringData<DataFormat::Gridded, DataFormat::Gridded>
+class ScatteringData<Scalar, DataFormat::Gridded>
     : public ScatteringDataBase<Scalar> {
  public:
   using CoeffType = Scalar;
@@ -216,20 +242,20 @@ class ScatteringData<DataFormat::Gridded, DataFormat::Gridded>
                  VectorType zenith_grid_incoming,
                  VectorType azimuth_grid_scattering,
                  VectorType zenith_grid_scattering,
-                 TensorType<7> phase_matrix_data,
-                 TensorType<7> extinction_matrix_data,
-                 TensorType<7> absorption_vector_data,
+                 TensorType<7> phase_matrix,
+                 TensorType<7> extinction_matrix,
+                 TensorType<7> absorption_vector,
                  TensorType<6> backscattering_coefficient,
                  TensorType<6> forwardscattering_coefficient)
-      : ScatteringDataBase(frequency_grid, temperature_grid),
+      : ScatteringDataBase<Scalar>(frequency_grid, temperature_grid),
         azimuth_grid_incoming_(azimuth_grid_incoming),
         zenith_grid_incoming_(zenith_grid_incoming),
         azimuth_grid_scattering_(azimuth_grid_scattering),
         zenith_grid_scattering_(zenith_grid_scattering),
-        phase_matrix_data_(phase_matrix_data),
-        extrinction_matric_data_(extinction_matrix_data),
-        absorption_vector_data_(absorption_vector_data),
-        backscattering_coefficient_data_(backscattering_coefficient),
+        phase_matrix_(phase_matrix),
+        extinction_matrix_(extinction_matrix),
+        absorption_vector_(absorption_vector),
+        backscattering_coefficient_(backscattering_coefficient),
         forwardscattering_coefficient_(forwardscattering_coefficient) {}
 
  protected:
@@ -239,12 +265,12 @@ class ScatteringData<DataFormat::Gridded, DataFormat::Gridded>
   eigen::Vector<Scalar> zenith_grid_scattering_;
 
   eigen::Tensor<CoeffType, 7>
-      phase_matrix_data_;  // f x T x elements x (inc. angles) x (scat. angles)
+      phase_matrix_;  // f x T x elements x (inc. angles) x (scat. angles)
   eigen::Tensor<CoeffType, 7>
-      extinction_matrix_data_;  // f x T x elements x (inc. angles) x (scat.
+      extinction_matrix_;  // f x T x elements x (inc. angles) x (scat.
                                 // angles)
   eigen::Tensor<CoeffType, 7>
-      absorption_vector_data_;  // f x T x elements x (inc. angles) x (scat.
+      absorption_vector_;  // f x T x elements x (inc. angles) x (scat.
                                 // angles)
   eigen::Tensor<CoeffType, 6>
       backscattering_coefficient_;  // f x T x elements x (inc. angles) x (scat.
@@ -255,7 +281,7 @@ class ScatteringData<DataFormat::Gridded, DataFormat::Gridded>
 };
 
 template <typename Scalar>
-class ScatteringData<DataFormat::Gridded, DataFormat::Spectral>
+class ScatteringData<Scalar, DataFormat::Spectral>
     : public ScatteringDataBase<Scalar> {
  public:
   using CoeffType = std::complex<Scalar>;
@@ -266,68 +292,73 @@ class ScatteringData<DataFormat::Gridded, DataFormat::Spectral>
   ScatteringData(VectorType frequency_grid, VectorType temperature_grid,
                  VectorType azimuth_grid_incoming,
                  VectorType zenith_grid_incoming,
-                 TensorType<6> phase_matrix_data,
-                 TensorType<6> extinction_matrix_data,
-                 TensorType<6> absorption_vector_data,
+                 TensorType<6> phase_matrix,
+                 TensorType<6> extinction_matrix,
+                 TensorType<6> absorption_vector,
                  TensorType<5> backscattering_coefficient,
                  TensorType<5> forwardscattering_coefficient)
-      : ScatteringDataBase(frequency_grid, temperature_grid),
+      : ScatteringDataBase<Scalar>(frequency_grid, temperature_grid),
         azimuth_grid_incoming_(azimuth_grid_incoming),
         zenith_grid_incoming_(zenith_grid_incoming),
-        phase_matrix_data_(phase_matrix_data),
-        extrinction_matric_data_(extinction_matrix_data),
-        absorption_vector_data_(absorption_vector_data),
-        backscattering_coefficient_data_(backscattering_coefficient),
+        phase_matrix_(phase_matrix_),
+        extinction_matrix_(extinction_matrix_),
+        absorption_vector_(absorption_vector_),
+        backscattering_coefficient_(backscattering_coefficient),
         forwardscattering_coefficient_(forwardscattering_coefficient) {}
  protected:
+  eigen::Vector<Scalar> azimuth_grid_incoming_;
+  eigen::Vector<Scalar> zenith_grid_incoming_;
   eigen::Tensor<CoeffType, 6>
-      phase_matrix_data;  // f x T x elements x (inc. angles) x nlm
+      phase_matrix_;  // f x T x elements x (inc. angles) x nlm
   eigen::Tensor<CoeffType, 6>
-      extinction_matrix_data;  // f x T x elements x (inc. angles) x nlm
+      extinction_matrix_;  // f x T x elements x (inc. angles) x nlm
   eigen::Tensor<CoeffType, 6>
-      absorption_vector_data;  // f x T x elements x (inc. angles) x nlm
+      absorption_vector_;  // f x T x elements x (inc. angles) x nlm
   eigen::Tensor<CoeffType, 5>
-      backscattering_coefficient;  // f x T x elements x (inc. angles) x nlm
+      backscattering_coefficient_;  // f x T x elements x (inc. angles) x nlm
   eigen::Tensor<CoeffType, 5>
-      forwardscattering_coefficient;  // f x T x elements x (inc. angles) x nlm
+      forwardscattering_coefficient_;  // f x T x elements x (inc. angles) x nlm
 };
 
+// pxx :: export
+// pxx :: instance("ScatteringDataFullySpectral", ["double"])
 template <typename Scalar>
-class ScatteringData<DataFormat::Spectral, DataFormat::Spectral>
+class ScatteringData<Scalar, DataFormat::FullySpectral>
     : public ScatteringDataBase<Scalar> {
  public:
   using CoeffType = std::complex<Scalar>;
   using VectorType = eigen::Vector<Scalar>;
+
+private:
   template <size_t rank>
       using TensorType = eigen::Tensor<CoeffType, rank>;
+public:
 
   ScatteringData(VectorType frequency_grid,
                  VectorType temperature_grid,
-                 TensorType<6> phase_matrix_data,
-                 TensorType<6> extinction_matrix_data,
-                 TensorType<6> absorption_vector_data,
+                 TensorType<6> phase_matrix,
+                 TensorType<6> extinction_matrix,
+                 TensorType<6> absorption_vector,
                  TensorType<5> backscattering_coefficient,
                  TensorType<5> forwardscattering_coefficient)
-      : ScatteringDataBase(frequency_grid, temperature_grid),
-        azimuth_grid_incoming_(azimuth_grid_incoming),
-        zenith_grid_incoming_(zenith_grid_incoming),
-        phase_matrix_data_(phase_matrix_data),
-        extrinction_matric_data_(extinction_matrix_data),
-        absorption_vector_data_(absorption_vector_data),
-        backscattering_coefficient_data_(backscattering_coefficient),
+      : ScatteringDataBase<Scalar>(frequency_grid, temperature_grid),
+        phase_matrix_(phase_matrix),
+        extinction_matrix_(extinction_matrix),
+        absorption_vector_(absorption_vector),
+        backscattering_coefficient_(backscattering_coefficient),
         forwardscattering_coefficient_(forwardscattering_coefficient) {}
 
  protected:
   eigen::Tensor<CoeffType, 6>
-      phase_matrix_data;  // f x T x elements x (nlm_inc) x nlm x 2
+      phase_matrix_;  // f x T x elements x (nlm_inc) x nlm x 2
   eigen::Tensor<CoeffType, 6>
-      extinction_matrix_data;  // f x T x elements x (nlm_inc) x nlm x 2
+      extinction_matrix_;  // f x T x elements x (nlm_inc) x nlm x 2
   eigen::Tensor<CoeffType, 6>
-      absorption_vector_data;  // f x T x elements x (nlm_inc) x nlm x 2
+      absorption_vector_;  // f x T x elements x (nlm_inc) x nlm x 2
   eigen::Tensor<CoeffType, 5>
-      backscattering_coefficient;  // f x T x elements x (nlm_inc) x nlm x 2
+      backscattering_coefficient_;  // f x T x elements x (nlm_inc) x nlm x 2
   eigen::Tensor<CoeffType, 5>
-      forwardscattering_coefficient;  // f x T x elements x (nlm_inc) x nlm x 2
+      forwardscattering_coefficient_;  // f x T x elements x (nlm_inc) x nlm x 2
 };
 
 }  // namespace scatlib
